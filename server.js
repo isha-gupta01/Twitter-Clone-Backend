a2147192ff360487cab9694fad183053a79254bf
Bloomer004 <ishaguptavnsv@gmail.com>
2025-03-07 20:09:07 +0530

    follow api

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "*", // Allow all origins (for testing)
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-}));
+app.use(cors());
 app.use('/trends/trendingtweets',trending);
 app.use('/api/auth', router); 
 app.use('/loggeduser', loggedUser); 
